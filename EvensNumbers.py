719905d2c0a26c326a6da76f7eb8018370ec0efb
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-10-25 17:38:31 +0530

    Print Given Number is Even or Odd

---
+++ EvensNumbers.py
@@ -0,0 +1,46 @@
+# Print Given Number is Even Or Odd Number
+
+n = eval(input('Enter A Number: '))
+if n%2==0:
+    print("{} Is Even Number".format(n))
+else:
+    print("{} Is Odd Number".format(n))
+
+
+
+
+# Print First 10 Even Numbers Or Postive Numbers
+
+for i in range(1,21):
+    if i % 2 == 0:
+        print("The Even Numbers are : ",i)
+
+
+
+
+
+# Print Even Numbers In 0 to 50 in List form
+
+list=[]
+for i in range(0,51,2):
+    list.append(i)
+
+print(list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
