91efc2072e31951aabebca746843cf1e60a2b4b2
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-10-27 11:20:47 +0530

    Reverse String Program

---
+++ ReverseString.py
@@ -0,0 +1,36 @@
+# Print The Reverse String
+
+str = "Sudhakar Reddy"
+str = str[::-1]
+print(str)
+
+
+# By Using Function
+
+def reverse_string(s):
+    return s[::-1]
+
+s="Gayatri"
+s=reverse_string(s)
+print(s)
+
+
+
+# Print Reverse Sentance
+
+intro = "Hello My Self Sudhakar Reddy, I have Past 2 years in Python/Django Developer"
+
+intro=intro[::-1]
+print(intro)
+
+
+# Print Reverse String Using Split Function
+
+selff = "Hi How Are You..! , Where You From"
+
+words = selff.split()
+words = words[::-1]
+print(words)
+
+
+
