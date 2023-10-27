760d0db88b34d8fe3affe7232dc080be965906fa
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-10-27 10:59:32 +0530

    Print The Prime Numbers 0 to 20

---
+++ PrimeNumber.py
@@ -0,0 +1,43 @@
+# Print The Prime Number 1 to 50 
+
+
+for i in range(1,51):
+    if i > 1:
+        for j in range(2,i):
+            if i % j == 0:
+                break
+        else:
+            print(i)
+
+
+
+
+# Print The Prime Number 1 to 20 The Result in List Form
+
+
+lst=[]
+for i in range(1,21):
+        for j in range(2,i):
+            if i % j == 0:
+                break
+        else:
+            lst.append(i)
+print(lst)
+
+
+
+# Check The Number is Prime Number or Not ?
+
+
+foo = int(input("Enter A Number : "))
+
+for i in range(2,foo):
+    if foo % 2 == 0:
+        print(f"{foo} is Not Prime Number..!")
+        break
+    else:
+        print(f"{foo} is Prime Number..!")
+
+
+
+
