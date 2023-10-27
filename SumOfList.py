6af19b9ecd4ff1677050c4ddcccc862065e34064
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-10-27 17:59:10 +0530

    Find The Sum Elements in List

---
+++ SumOfList.py
@@ -0,0 +1,21 @@
+# Find Sum Of Elements List
+
+"""
+    Sum Of Elements mean total sum in list
+"""
+
+list = [1,2,3,4,5]
+print(sum(list))
+
+# add one more value to output
+
+list = [1,2,3,4,5]
+print(sum(list,12))
+
+
+
+# substact one more value to output
+
+list = [1,2,3,4,5]
+print(sum(list,-10))
+
