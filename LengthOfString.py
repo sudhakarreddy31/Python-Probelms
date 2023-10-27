5b122fb7f75c5903a57bf17ba2c79191fa6f9c25
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-10-27 11:39:13 +0530

    Write Program For Given String Find Length of Each Word .

---
+++ LengthOfString.py
@@ -0,0 +1,36 @@
+# Find The Lenth of Given String ?
+
+
+list=[]
+str = "Welcome Came To Artfical Inteligenace World..!"
+str1 = str.split()
+print(str1)
+for i in str1:
+    x=len(i)
+    list.append(x)
+print(list)
+
+
+lst=[]
+sen="Moreover you will learn how to declare and slice a string in python and also look at the Python String functions and Python String operations."
+sent=sen.split()
+print(sent)
+
+for i in sent:
+    x=len(i)
+    lst.append(x)
+print(lst)
+
+
+
+
+list=[]
+name = "Sudhakar Reddy Moola"
+Name = name.split()
+print(Name)
+
+for i in Name:
+    length = len(i)
+    list.append(length)
+
+print(list)
