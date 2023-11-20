b0a7d5769582dcbe73b3ae3f52fde72be207e86d
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-11-20 12:36:55 +0530

    String Problems

---
+++ StringsProblems/StringContainsOnlyDigits.py
@@ -0,0 +1,15 @@
+# How do you check if a string contains only digits? 
+
+
+string = "431418"
+str=string.isdigit()
+print(str)
+
+str = "431.418"
+string=str.isdigit()
+print(string)
+
+
+stringg = "123msr"
+str1=stringg.isdigit()
+print(str1)
