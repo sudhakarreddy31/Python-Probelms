b5e913bdbc10cb5ca994f58dc4e5f35ff1d19bc3
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-11-20 12:32:41 +0530

    Calculate Square Root Of Given Numbers

---
+++ CalculateSquareOfNumber.py
@@ -0,0 +1,17 @@
+# How to calculate the square root of a given number?
+
+# number = int(input("Enter a number:"))
+
+# result = number ** 0.5
+
+# print(result)
+
+# import math
+
+# number = 25
+# result = math.sqrt(number)
+# print(result)
+
+
+
+
