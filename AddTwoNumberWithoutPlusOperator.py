9f1af37a007f5cfb66fd43d3d86646770477e82f
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-11-20 12:31:33 +0530

    Added Two Numbers Without Using Plus Operators

---
+++ AddTwoNumberWithoutPlusOperator.py
@@ -0,0 +1,8 @@
+# How to add two numbers without using the plus operator
+
+
+number1 = int(input("Enter First Number:"))
+number2 = int(input("Enter Second Number:"))
+
+add = number1 - (-number2)
+print("Add Two Numbers Without using plus operator:",add)
