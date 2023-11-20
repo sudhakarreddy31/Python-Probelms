c8cff76e45c54111050024219d473574c7b790ee
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-11-20 12:35:51 +0530

    Pastive or Negative Numbers

---
+++ PastiveOrNegativeNumbers.py
@@ -0,0 +1,16 @@
+# How to check if a given number is positive or negative in Python?
+
+
+number = eval(input("Enter A Number: "))
+
+if number > 0:
+    print(f"{number} is a pastive Number..")
+elif number < 0:
+    print(f"{number} is Negative Number..")
+else:
+    print(f"{number} is Zero")
+
+
+
+
+
